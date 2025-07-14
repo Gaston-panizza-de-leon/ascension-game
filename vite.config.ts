2d13d8c733930b250587e072bcbd934b401f5996
Gaston-panizza-de-leon <lucas-gaston.panizza1@estudiant.uib.eu>
2025-07-14 16:47:45 +0200

    Path problems

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ascension-game/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets') // Define un alias para tu carpeta de assets
+    }
+  }
 })
